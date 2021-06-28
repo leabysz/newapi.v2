5498d97753cb153d7efc7f908eeca468817743ca
Bruno Paulette <brunopaulettecurbelo@gmail.com>
2021-06-28 09:31:37 -0300

    nueva hora

--- src/api.js
+++ src/api.js
@@ -29,7 +29,7 @@ router.get("/newsapi", (req, res) => {
     res.send(newsData);
 });
 
-const getNewsJob = schedule.scheduleJob('15 * * * *', function () {
+const getNewsJob = schedule.scheduleJob('35 * * * *', function() {
     console.log('Trae las noticias en el minuto N de la hora')
     axios.get(`${newsApiRoute}${newsApiKey}`)
       .then((response) => newsData=response.data)
